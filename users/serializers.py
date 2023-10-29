e6c47266b12d03d3a5c40c3512d6d94f1da335c4
Acetonerer <bolokan2014@gmail.com>
2023-10-29 12:36:14 +0700

    fix users app

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-
+from courses.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializers(serializers.ModelSerializer):
+    payment = PaymentsSerializer(many=True)
+
     class Meta:
         model = User
         fields = '__all__'
