e94ff197496d3979a7da4660f112903720f81c5e
Acetonerer <bolokan2014@gmail.com>
2023-10-29 12:36:34 +0700

    fix courses app

--- courses/urls.py
+++ courses/urls.py
@@ -1,21 +1,18 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-
 from courses.apps import CoursesConfig
-
-from courses.views import *
+from courses.views import (CourseViewSet, LessonCreateView, LessonRetrieveView, LessonUpdateView,
+                           LessonListView,  LessonDestroyView, PaymentsListAPIView)
 
 app_name = CoursesConfig.name
 
 router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
-
 urlpatterns = [
     path('lesson/create', LessonCreateView.as_view(), name='lesson_create'),
     path('lesson/', LessonListView.as_view(), name='lesson_list'),
     path('lesson/update/<int:pk>/', LessonUpdateView.as_view(), name='lesson_update'),
     path('lesson/delete/<int:pk>/', LessonDestroyView.as_view(), name='lesson_delete'),
-    path('lesson/<int:pk>/', LessonRetrieveView.as_view(), name='lesson_detail')
-
+    path('lesson/<int:pk>/', LessonRetrieveView.as_view(), name='lesson_detail'),
+    path('payment/', PaymentsListAPIView.as_view(), name='payment-list'),
 ] + router.urls
-
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets, generics
-
-from courses.models import Course, Lesson
-from courses.serializers import CourseSerializers, LessonSerializers
+from courses.models import Course, Lesson, Payments
+from courses.serializers import CourseSerializers, LessonSerializers, PaymentsSerializer, CourseCreateSerializers
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    serializer_class = CourseSerializers
+    default_serializer_class = CourseSerializers
     queryset = Course.objects.all()
+    serializers = {
+        'create': CourseCreateSerializers,
+    }
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.default_serializer_class)
 
 
 class LessonCreateView(generics.CreateAPIView):
@@ -31,3 +38,11 @@ class LessonListView(generics.ListAPIView):
 class LessonDestroyView(generics.DestroyAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+
+
+class PaymentsListAPIView(generics.ListAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'method',)
+    ordering_fields = ('date_payment',)
