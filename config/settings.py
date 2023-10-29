d1892bd5ae81e5e26070fe717d218f29f2a59e41
Acetonerer <bolokan2014@gmail.com>
2023-10-29 12:37:13 +0700

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'users',
     'courses',
     'rest_framework',
+
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
