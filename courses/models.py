e0d1db61e5b916364ec9fc581456a69ec6b26575
Acetonerer <bolokan2014@gmail.com>
2023-11-09 18:27:15 +0700

    fix courses (25.1)

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,5 @@
 from config import settings
 from django.db import models
-
-
 NULLABLE = {'blank': True, 'null': True}
 PAYMENT_CHOICES = (
     ('transfer to account', 'перевод на счет'),
@@ -13,7 +11,6 @@ class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
     picture = models.ImageField(upload_to='course/', verbose_name='Изображение', **NULLABLE)
-
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
                               verbose_name='Пользователь', **NULLABLE)
 
@@ -31,8 +28,9 @@ class Lesson(models.Model):
     image = models.ImageField(upload_to='lesson/', verbose_name='Изображение', **NULLABLE)
     url = models.URLField(max_length=250, verbose_name='ссылка', **NULLABLE)
 
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Урок',
-                               **NULLABLE, related_name='lesson')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Урок', **NULLABLE, related_name='lesson')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='Пользователь',
+                              **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
